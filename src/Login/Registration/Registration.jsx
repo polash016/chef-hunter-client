782a9024d57a2138665d1f7e720884d096dac177
polash016 <robin.shrkr@gmail.com>
2023-05-03 15:02:18 +0600

    registration form created

--- src/Login/Registration/Registration.jsx
+++ src/Login/Registration/Registration.jsx
@@ -1,10 +1,72 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Registration = () => {
+    const handleRegistration = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+    }
     return (
-        <div>
-            
-        </div>
+        <Container className="w-25">
+      <Form onSubmit={handleRegistration}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            type="text"
+            name="photo"
+            placeholder="Photo URL"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check
+          onClick={handleAccept}
+            type="checkbox"
+            name="accept"
+            label="Accept Terms & Conditions"
+          />
+        </Form.Group>
+        <Button variant="primary" disabled={!accepted} type="submit">
+          Register
+        </Button>
+        <br />
+        <Form.Text className="text-muted">
+          Already Have An Account? <Link to="/login">Login</Link>
+        </Form.Text>
+        <Form.Text className="text-danger"></Form.Text>
+        <Form.Text className="text-success"></Form.Text>
+      </Form>
+    </Container>
     );
 };
 
